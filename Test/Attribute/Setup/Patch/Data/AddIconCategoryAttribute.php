6a830e9b294354e61c4fe0aeabcc8ebf15e1dadf
Amit Vaghela <88328183+amit-s-vaghela@users.noreply.github.com>
2023-07-21 15:28:49 +0530

    Add files via upload

---
+++ Test/Attribute/Setup/Patch/Data/AddIconCategoryAttribute.php
@@ -0,0 +1,85 @@
+<?php
+declare (strict_types = 1);
+
+namespace Test\Attribute\Setup\Patch\Data;
+
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Catalog\Model\Category;
+
+/**
+ * Class CategoryAttribute for Create Custom Category Attribute using Data Patch.
+ */
+class AddIconCategoryAttribute implements DataPatchInterface 
+{
+
+    /**
+     * ModuleDataSetupInterface
+     *
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * EavSetupFactory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory          $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply() {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute(Category::ENTITY,
+            'icon',
+            [
+                'type' => 'varchar',
+                'label' => 'Icon',
+                'input' => 'image',
+                'sort_order' => 333,
+                'source' => '',
+                'global' => ScopedAttributeInterface::SCOPE_STORE,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => null,
+                'group' => 'General Information',
+                'backend' => 'Magento\Catalog\Model\Category\Attribute\Backend\Image'
+            ]
+        );
+ 
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies() {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases() {
+        return [];
+    }
+}
